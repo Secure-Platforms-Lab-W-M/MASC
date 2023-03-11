80306a6a535ef60766db62339468265201ce8c42
scttmars <55801303+scttmars@users.noreply.github.com>
2023-03-11 14:20:48 -0500

    Fixed some bugs in interproc addition
    Interproc addition would only work with misuses that were three letters long. Fixed this issue to create however many methods for number of letters

--- masc-core/app/src/main/java/edu/wm/cs/masc/mutation/builders/restrictive/BuilderInterprocAdditionClass.java
+++ masc-core/app/src/main/java/edu/wm/cs/masc/mutation/builders/restrictive/BuilderInterprocAdditionClass.java
@@ -12,14 +12,14 @@ public class BuilderInterprocAdditionClass {
 //            }
         String insecure = p.getInsecureParam();
 
-        String template =
+        String classStart =
                 "public class %1$s {\n" +
                         "    private String %2$s = \"%3$s\";\n" +
                         "    public %1$s A(){\n" +
                         "        %2$s = \"%3$s\";\n" +
                         "        return this;\n" +
-                        "    }\n" +
-                        "    public %1$s B(){\n" +
+                        "    }\n";
+                        /*"    public %1$s B(){\n" +
                         "        return \"%4$s\";\n" +
                         "    }\n" +
                         "    public %1$s C(){\n" +
@@ -31,14 +31,29 @@ public class BuilderInterprocAdditionClass {
                         "    public String add%2$s(){\n" +
                         "        %2$s = B() + C() + D();\n" +
                         "        return this;\n" +
-                        "    }\n" +
-                        "    public String get%2$s(){\n" +
-                        "        return %2$s;\n" +
-                        "    }\n" +
-                        "}";
+                        "    }\n" + */
+        String addMethods = "";
+        String methods = "";
+                        for (int i = 0; i < insecure.length(); i++){
+                            methods = methods +
+                                    "    public String " + insecure.charAt(i) + "(){\n" +
+                                    "        return \"" +  insecure.charAt(i) + "\";\n" +
+                                    "    }\n";
+                            addMethods = addMethods + insecure.charAt(i) + "() + ";
 
-        sb.append(String.format(template, p.getOtherClassName(), p.getPropertyName(), p.getSecureParam(),
-                p.getInsecureParam().charAt(0),p.getInsecureParam().charAt(1),p.getInsecureParam().charAt(2)));
+                        }
+                        addMethods = addMethods.substring(0,addMethods.length() - 3);
+                        methods = methods +
+                                "    public String add%2$s(){\n" +
+                                "        %2$s = " + addMethods + ";\n" +
+                                "        return this;\n" +
+                                "    }\n" +
+                                "    public String get%2$s(){\n" +
+                                "        return %2$s;\n" +
+                                "    }\n" +
+                                "}";
+
+        sb.append(String.format(classStart + methods, p.getOtherClassName(), p.getPropertyName(), p.getSecureParam()));
         return sb.toString();
 
     }
